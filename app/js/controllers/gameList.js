ff90d2df41d94cb455ecf0122262797507543802
robhendriks <cyanidenl@icloud.com>
2016-06-13 02:20:35 +0200

    sexy UI is sexy

--- app/js/controllers/gameList.js
+++ app/js/controllers/gameList.js
@@ -4,10 +4,16 @@ module.exports = function($scope, GameFactory) {
 	$scope.user = null;
 	$scope.games = [];
 
+	$scope.state = 'playing';
+
 	$scope.init = function() {
 		GameFactory.GET(function(games) {
 			$scope.games = games;
 		});
+	};
+
+	$scope.filterState = function() {
+		return $scope.state !== null;
 	};
 
 	$scope.init();
